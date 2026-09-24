c48038f450ef90980ad9d48fada49607900e9dac
JasonLiu <jasonliudehui227@gmail.com>
2026-09-24 14:46:59 +0100

    Defined CRUD functions in TripStore.kt

--- app/src/main/java/com/example/personal_holiday_planner/stores/TripStore.kt
+++ app/src/main/java/com/example/personal_holiday_planner/stores/TripStore.kt
@@ -1,4 +1,10 @@
 package com.example.personal_holiday_planner.stores
+import com.example.personal_holiday_planner.models.TripModel
 
-class TripStore {
+interface TripStore {
+    fun findAll(): List<TripModel>
+    fun create(trip: TripModel)
+    fun update(trip: TripModel): Boolean
+    fun delete(id: Long): Boolean
+    fun findOne(id: Long): TripModel?
 }
