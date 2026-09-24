dc20eb7dc92f985e285ae659f6c2fcd721046b19
JasonLiu <jasonliudehui227@gmail.com>
2026-09-24 14:40:51 +0100

    Variables made for trip model

--- app/src/main/java/com/example/personal_holiday_planner/models/TripModel.kt
+++ app/src/main/java/com/example/personal_holiday_planner/models/TripModel.kt
@@ -1,4 +1,9 @@
 package com.example.personal_holiday_planner.models
 
-class TripModel {
-}
+class TripModel (
+    var id: Long = 0L,
+    var destination: String = "",
+    var startDate: String = "",
+    var endDate: String = "",
+    var budget: Double = 0.0
+    )
