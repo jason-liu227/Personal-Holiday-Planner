df6b13aeb3e629fc1b03f23b6371765e7b362801
JasonLiu <jasonliudehui227@gmail.com>
2026-09-24 14:51:51 +0100

    Added findAll for trip

--- app/src/main/java/com/example/personal_holiday_planner/stores/TripMemStore.kt
+++ app/src/main/java/com/example/personal_holiday_planner/stores/TripMemStore.kt
@@ -1,4 +1,13 @@
 package com.example.personal_holiday_planner.stores
 
+import com.example.personal_holiday_planner.models.TripModel
+
 class TripMemStore {
+
+    private val trips = ArrayList<TripModel>()
+
+    fun findAll(): List<TripModel> {
+        return trips
+    }
+
 }
