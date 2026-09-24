d8c7b26b39f923028fad83c2ae040f38c06cc5ca
JasonLiu <jasonliudehui227@gmail.com>
2026-09-24 14:54:23 +0100

    Added create function for trip

--- app/src/main/java/com/example/personal_holiday_planner/stores/TripMemStore.kt
+++ app/src/main/java/com/example/personal_holiday_planner/stores/TripMemStore.kt
@@ -1,13 +1,20 @@
 package com.example.personal_holiday_planner.stores
 
 import com.example.personal_holiday_planner.models.TripModel
+import java.util.concurrent.atomic.AtomicLong
 
 class TripMemStore {
 
     private val trips = ArrayList<TripModel>()
+    private val lastId = AtomicLong(0L)
 
     fun findAll(): List<TripModel> {
         return trips
+    }
+
+    fun create(trip: TripModel) {
+        trip.id = lastId.incrementAndGet()
+        trips.add(trip)
     }
 
 }
